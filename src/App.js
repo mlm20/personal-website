244abdce125517f274283af4c2894dc28429a603
Max <mlm20@ic.ac.uk>
2022-05-21 18:44:39 +0100

    eslint + tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-function App () {
+import React from "react";
+
+const App = function () {
     return (
         <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
             <p className="text-3xl text-gray-700 font-bold mb-5">
@@ -9,6 +11,6 @@ function App () {
             </p>
         </div>
     );
-}
+};
 
 export default App;
